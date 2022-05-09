9bd3a50e0c284ff706f800aec5e29e7ba0b51173
socheatos <chansocheatasea@gmail.com>
2022-05-08 21:43:08 -0400

    evaluates distraction based on frame count instead of time count

--- distraction_detection.py
+++ distraction_detection.py
@@ -5,27 +5,24 @@ import cv2
 # https://github.com/e-candeloro/Driver-State-Detection/blob/master/driver_state_detection/Driver_State_Detection.py
 
 class DistractionScore():
-    def __init__(self,fps_lim, ROLL_THRESH, PITCH_THRESH , YAW_THRESH, POSE_TIME_THRESH = 4.0):
+    def __init__(self, ROLL_THRESH, PITCH_THRESH , YAW_THRESH, POSE_FRAME_THRESH = 50.0):
         
-        self.fps = fps_lim          # upper frame rate of video stream considered
-        self.delta_time_frame = 1/fps_lim
-        self.prev_time = 0
 
         self.ROLL_THRESH = ROLL_THRESH
         self.PITCH_THRESH = PITCH_THRESH
         self.YAW_THRESH = YAW_THRESH
 
-        self.pose_time_thresh = POSE_TIME_THRESH    # max time allowable for consecutive distracted headpose 
-        self.pose_act_tresh = self.pose_time_thresh / self.delta_time_frame
+        self.FRAME_THRESH = POSE_FRAME_THRESH   
+
         self.pose_counter = 0
 
     def evaluate(self, pitch, roll, yaw):
         distracted = False 
 
-        if self.pose_counter >= self.pose_act_tresh:
+        if self.pose_counter >= self.FRAME_THRESH:
             distracted=True
         
-        if ((abs(roll) < self.ROLL_THRESH) or (abs(pitch) > self.PITCH_THRESH) or (abs(yaw) > self.YAW_THRESH)):
+        if ((abs(roll) > self.ROLL_THRESH) or (abs(pitch) > self.PITCH_THRESH) or (abs(yaw) > self.YAW_THRESH)):
             if not distracted:
                 self.pose_counter += 1
                 
--- main.py
+++ main.py
@@ -5,37 +5,31 @@ from distraction_detection import DistractionScore
 import cv2, time
 
 def main():
-    curr_time = 0 
-    pst_time = 0 
-    prev_time = 0 
-    fps_lim = 11
-    time_lim = 1/fps_lim
-
     # instantiate our objects
     vid = Video(0)
     detection = Detection(vid)
     pose = Pose()   
-    scorer = DistractionScore(fps_lim,  PITCH_THRESH=20,YAW_THRESH=35, ROLL_THRESH=20, POSE_TIME_THRESH=3.0)
+    scorer = DistractionScore(PITCH_THRESH=5,YAW_THRESH=5, ROLL_THRESH=20, POSE_FRAME_THRESH=50.0)
 
+    distracted_count = 0
+    count_frame = 0
     while True: 
-        delta = time.time()
+        count_frame+=1
+        fps = vid.vid.get(cv2.CAP_PROP_FPS)
         vid.get_frame()
-
-        # if time passed is bigger or equal than frame time process the frame
-        if delta >= time_lim:
-            prev_time = time.time()
-            # compute actual fps of webcam vid stearm
-            curr_time = time.time()
-            fps = 1.0/float(curr_time-pst_time)
-            pst_time = curr_time
-            cv2.putText(vid.img, "FPS: " + str(round(fps,0)), (50, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-            detection.detect_landmarks(show='HPE')
-            pitch, yaw, roll = pose.estimate(detection=detection, camera=vid)
-            distracted = scorer.evaluate(pitch, yaw, roll)
-
-            if distracted:
-                cv2.putText(vid.img, "DISTRACTED", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 2)
+    
+        cv2.putText(vid.img, "Frame: " + str(count_frame), (50, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        cv2
+        detection.detect_landmarks(show='HPE')
+        pitch, yaw, roll = pose.estimate(detection=detection, camera=vid,allpts=True)
+        distracted = scorer.evaluate(pitch, yaw, roll)
+        # print(distracted)
+        if distracted:
+            distracted_count+=1
+        elif distracted_count > 0: 
+            distracted_count-=fps
+        if distracted_count>50:
+            cv2.putText(vid.img, "DISTRACTED", (45, 55), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 5)
         
         vid.show_frame()
         if cv2.waitKey(1) & 0xFF == 27:
